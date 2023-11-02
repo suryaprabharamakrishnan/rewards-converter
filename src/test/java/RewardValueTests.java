6cfbc4ee6c7fd3512906bad2298807c06c9c9ef1
Suryaprabha R <33887093+suryaprabharamakrishnan@users.noreply.github.com>
2023-11-02 18:41:48 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / RewardValue.CONVERSION_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int mileValue = 100;
+        double expectedCashValue = mileValue * RewardValue.CONVERSION_RATE;
+        var rewardValue = new RewardValue(mileValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
